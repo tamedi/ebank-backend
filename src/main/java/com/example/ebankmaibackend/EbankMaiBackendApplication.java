68fa7d481d2ded3add7f867cb29c59d2e018592c
mtamedi <mohamed-ilias.tamedi@capgemini.com>
2024-05-02 16:16:51 +0200

    first commit

---
+++ src/main/java/com/example/ebankmaibackend/EbankMaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebankmaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankMaiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankMaiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ebankmaibackend/EbankMaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebankmaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankMaiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
